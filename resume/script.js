afc6e0996b8b66891739861add8d95a46716298b
Tim <t-winsley@protonmail.com>
2024-11-07 23:26:40 -0500

    Fix API Url

--- resume/script.js
+++ resume/script.js
@@ -1,21 +1,21 @@
 function GetValueFromDB() {
-    let counter = document.getElementById('count');
-    let response;
-    const API_URL = "test.com"
-    fetch(API_URL, {
-  method: "POST",
-  body: JSON.stringify({
-    count: 1
-  }),
-  headers: {
-    "Content-type": "application/json; charset=UTF-8"
-  }
-})
-  .then((response) => response.json())
-  .then((json) => console.log(json))
-  .then((json) => response);
+  let counter = document.getElementById("count");
+  let response;
+  const API_URL = "https://functwcus0cloudresume1.azurewebsites.net";
+  fetch(API_URL, {
+    method: "POST",
+    body: JSON.stringify({
+      count: 1,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json))
+    .then((json) => response);
 
-  counter = response.map('count')
+  counter = response.map("count");
 }
 
 GetValueFromDB();
